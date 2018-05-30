3801d879d783294220e6c96c5c3cddc302fee4f7
André Martinelli <andremartinelli23@gmail.com>
2018-05-30 01:37:36 -0300

    removed commentaries

--- jvas.js
+++ jvas.js
@@ -1,8 +1,6 @@
 const cartinhas = 'https://api.magicthegathering.io/v1/cards'
 
 function criaCarta(nome, desc, URLimg, tit, i){
-    // var newDiv = document.createElement('div')
-    // newDiv.classList.add('container')
     var newRow = document.createElement('div')
     newRow.classList.add('row')
     var newCol = document.createElement('div')
@@ -27,14 +25,11 @@ function criaCarta(nome, desc, URLimg, tit, i){
     conteudo.appendChild(titulo)
     conteudo.appendChild(de)
     newCard.appendChild(conteudo)
-    // imagem.appendChild(conteudo)
     newCard.appendChild(imagem)
     newCol.appendChild(newCard)
     newRow.appendChild(newCol)
-    // newDiv.appendChild(newRow)
     var insere = document.getElementById('carta')
     insere.appendChild(newCol)
-    // document.body.insertBefore(newCol,insere)
 
 }
 
@@ -47,7 +42,6 @@ fetch(cartinhas, {
     return response.json()
 })
 .then(function(jsonrsp) {
-    console.log(jsonrsp.cards)
     for(var i = 0; i < jsonrsp.cards.length; i++) {
         criaCarta(jsonrsp.cards[i].name, jsonrsp.cards[i].originalText, jsonrsp.cards[i].imageUrl, jsonrsp.cards[i].originalType)
     }
